36d3f8a524a2ff3ac19a8d7616216c3cbe35003a
WiktorIdziak <Witusaki@gmail.com>
2021-11-24 13:49:23 +0100

    Initial commit

---
+++ src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+
+public class Calculator {
+
+    double dodawanie(double x1, double x2) {
+        return x1 + x2;
+    }
+
+    double odejmowanie(double x1, double x2) {
+        return x1 - x2;
+    }
+
+}
